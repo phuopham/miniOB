d67827c20980e0a8b82f1808e41ea4445f996099
phuopham <108047552+phuopham@users.noreply.github.com>
2023-04-19 22:57:55 +0700

    fix cart display issue in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -18,41 +18,46 @@
                 </div>
             </div>
         </div>
-        @if ($cart->customer != '' && !$cart->product != [])
-        <div class=" col-12 col-md-6 col-xl-4 bg-teal shadow-lg rounded rounded-3 " {{ request()->is('cart') ? 'hidden'
-            : '' }}>
-            {{-- {{ json_encode($cart) }} --}}
-            <div class="d-flex align-items-center justify-content-between">
-                <h4>Cart</h4>
-                <a class="btn btn-secondary" onclick="return confirm('Are you sure you want to delete current cart?')"
-                    href="{{ route('cart.cancel') }}">Hủy đơn</a>
-                <a href="{{ route('cart.index') }}"><u>Tới đơn hàng -&gt;</u></a>
-            </div>
-            @if ($cart->customer != '')
-            <div>Tên khách: {{ json_decode($cart->customer)->name }}</div>
-            @endif
-            <div>Sản phẩm</div>
-            @if ($cart->products != [])
-            <ul>
-                @foreach (json_decode($cart->products) as $product)
-                <li class="mb-2">
-                    <form class="d-inline" action="{{ route('cart.removeProduct') }}" method="post">
-                        @csrf
-                        <input type="text" name="product" hidden value='{{ json_encode($product) }}'>
+        {{-- {{ json_encode($cart) }} --}}
+        @if ($cart->customer != '' || $cart->products != '[]')
+            <div class=" col-12 col-md-6 col-xl-4 bg-teal shadow-lg rounded rounded-3 "
+                {{ request()->is('cart') ? 'hidden' : '' }}>
+                <div class="d-flex align-items-center justify-content-between">
+                    <h4>Cart</h4>
+                    <a class="btn btn-secondary"
+                        onclick="return confirm('Are you sure you want to delete current cart?')"
+                        href="{{ route('cart.cancel') }}">Hủy đơn</a>
+                    <a href="{{ route('cart.index') }}"><u>Tới đơn hàng -&gt;</u></a>
+                </div>
+                @if ($cart->customer != '')
+                    <div>Tên khách: {{ json_decode($cart->customer)->name }}</div>
+                @else
+                    <div>Thêm <a href="{{ route('customers.index') }}">khách hàng</a></div>
+                @endif
+                <div>Sản phẩm</div>
+                @if ($cart->products != '[]')
+                    <ul>
+                        @foreach (json_decode($cart->products) as $product)
+                            <li class="mb-2">
+                                <form class="d-inline" action="{{ route('cart.removeProduct') }}" method="post">
+                                    @csrf
+                                    <input type="text" name="product" hidden value='{{ json_encode($product) }}'>
 
-                        <button class="btn btn-danger" type="submit">Xóa</button>
-                    </form>
-                    {{ $product->name }} - {{ $product->quantity }}
-                    <form class="d-inline" action="{{ route('cart.reduceQuantity') }}" method="post">
-                        @csrf
-                        <input type="text" name="product" hidden value='{{ json_encode($product) }}'>
-                        <button class="btn btn-secondary px-3">-</button>
-                    </form>
-                </li>
-                @endforeach
-            </ul>
-            @endif
-        </div>
+                                    <button class="btn btn-danger" type="submit">Xóa</button>
+                                </form>
+                                {{ $product->name }} - {{ $product->quantity }}
+                                <form class="d-inline" action="{{ route('cart.reduceQuantity') }}" method="post">
+                                    @csrf
+                                    <input type="text" name="product" hidden value='{{ json_encode($product) }}'>
+                                    <button class="btn btn-secondary px-3">-</button>
+                                </form>
+                            </li>
+                        @endforeach
+                    </ul>
+                @else
+                    <div>Thêm <a href="{{ route('products.index') }}">sản phẩm</a></div>
+                @endif
+            </div>
         @endif
     </div>
 </section>
